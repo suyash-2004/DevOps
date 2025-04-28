9e11093bda4ff730dea699204cccd46320fa8471
suyash-2004 <suyash096@gmail.com>
2025-04-28 11:11:27 +0530

    Devops Assignment

---
+++ DevOps_Test.java
@@ -0,0 +1,25 @@
+public class DevOps_Test { 
+    public static void main(String args[]) {
+        System.out.println("Hello, DevOps!");
+    }
+
+    //build a sample project
+    public static void buildProject() {
+        System.out.println("Building the project...");
+    }
+
+    //deploy a sample project
+    public static void deployProject() {
+        System.out.println("Deploying the project...");
+    }
+
+    //test a sample project
+    public static void testProject() {
+        System.out.println("Testing the project...");
+    }
+
+    //monitor a sample project
+    public static void monitorProject() {
+        System.out.println("Monitoring the project...");
+    }
+}
